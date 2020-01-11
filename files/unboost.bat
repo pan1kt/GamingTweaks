afef315efc602fbf9339ac1f86b9ee32fe15e38f
Felipe8581 <58223527+Felipe8581@users.noreply.github.com>
2020-01-11 03:47:25 +0000

    Update unboost.bat

--- files/unboost.bat
+++ files/unboost.bat
@@ -2,6 +2,12 @@ wmic process where name="steam.exe" call setpriority "normal"
 wmic process where name="steamwebhelper.exe" call setpriority "normal"
 wmic process where name="steamservice.exe" call setpriority "normal"
 wmic process where name="gameoverlayui.exe" call setpriority "normal"
+powercfg.exe /setactive 77777777-7777-7777-7777-777777777777
 sc start NVDisplay.ContainerLocalSystem
 sc start sppsvc
-powercfg.exe /setactive 77777777-7777-7777-7777-777777777777
+sc start nsi
+sc start NlaSvc
+sc start netprofm
+sc start Netman
+sc start AeLookupSvc
+sc start seclogon
